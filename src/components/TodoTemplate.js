fa27594d5cc1b603cd30f5711d35d76c58507bab
najaeseong1 <skwotjd5576@gmail.com>
2023-11-23 16:37:06 +0900

    2023-11-23 할일 체크 기능, 시간 표시 완료

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -67,13 +67,35 @@ const TodoTemplate = () => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  // 할 일 체크 처리 함수
+  const checkTodo = (id) => {
+    /*
+    const copyTodos = [...todos];
+    for (let cTodo of copyTodos) {
+      if (cTodo.id === id) {
+        cTodo.done = !cTodo.done;
+      }
+    }
+    setTodos(copyTodos);
+    */
+
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  };
+
+  // 체크가 안 된 할 일의 개수 카운트 하기
+  // 배열안에 객체가 있는데 done이 false인 객체만 count
+  const countRestTodo = () => {
+    return todos.filter((todo) => !todo.done).length;
+  };
+
   return (
     <div className='TodoTemplate'>
-      <TodoHeader />
-      <TodoMain
-        todoList={todos}
-        remove={removeTodo}
-      />
+      <TodoHeader count={countRestTodo} />
+      <TodoMain todoList={todos} remove={removeTodo} check={checkTodo} />
       <TodoInput addTodo={addTodo} />
     </div>
   );
